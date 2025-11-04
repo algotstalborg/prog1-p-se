89d902a99f497cbdc39937dffd23c84b02220cf5
algotstalborg <algot.stalborg@elev.ga.ntig.se>
2025-11-04 13:40:42 +0100

    silly

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+def tabortsistavärdet(lista):
+    lista.pop(len(lista))
+
 kör = True
 
 påse = []
@@ -10,10 +13,10 @@ while kör:
         print("avsluta [Q]")
         print("lägg till [G]")
         print("Sortera [S]")
-
+        print("Ta bort [T]")
     elif val.lower() in ("lista", "l"):
         for i in range(len(påse)):
-            print(f"{påse[i]}", end=" ")
+            print(f"{påse[i]}", end="")
             print("")
     elif val.lower() in ("lägg till", "g"):
         påse.append(input())
@@ -21,3 +24,12 @@ while kör:
         påse.sort()
     elif val.lower() in ("avsluta", "q"):
         kör = False
+    elif val.lower() in ("ta bort", "t"):
+        ta_bort_val = input("vill du ta bort ett värde [v] eller en position [p] eller sista värdet [s]?")
+        if ta_bort_val in ("värde", "v"):
+            påse.remove(input("vad vill du ta bort?"))
+        elif ta_bort_val in ("position", "p"): 
+            påse.pop(int(input("vilken position vill du ta bort? (från 1)"))-1)
+        elif ta_bort_val in ("sista", "s"):
+            tabortsistavärdet(påse)
+    else: print("eeh lär dig stava typ")
